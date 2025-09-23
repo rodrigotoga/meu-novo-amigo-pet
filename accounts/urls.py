1c9dd06b4a1fcc592cbc3db217eb269168c8786b
rodrigotoga <trodrigo350gmail.com>
2025-09-22 23:55:23 -0300

    tentando arrumar o banco de dados

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
+from .views import UsuarioRegistrationView
+
+
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('cadastrar/', UsuarioRegistrationView.as_view(), name='cadastrar'),
     path('register/', views.UsuarioRegistrationView.as_view(), name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
